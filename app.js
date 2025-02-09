9380080bc9525d34174650ebdfac13c467bb49d5
Shivashnu Rana <shivashnurana@gmail.com>
2025-02-10 01:08:19 +0530

    updates

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".h-list");
+    const body = document.querySelector("body");
+    const themeBtn = document.querySelector("#theme-switch");
+    const volBtn = document.querySelector("#vol-switch");
 
     burger.addEventListener("click", () => {
-        console.log("clicked");
         nav.classList.toggle("nav-active");
+    });
+
+    themeBtn.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+    });
+
+    volBtn.addEventListener("click", () => {
+        body.classList.toggle("vol");
     })
 }
 
