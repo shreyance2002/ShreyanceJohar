ff586752b20dc0615b3340545f5dd4d217ff6c28
Shivashnu Rana <shivashnurana@gmail.com>
2025-03-27 02:19:24 +0530

    Animation changes
    Home page animations done using gsap

--- animations.js
+++ animations.js
@@ -1,7 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const splitTypes = document.querySelectorAll(".abt-txt");
-console.log(splitTypes);
 
 splitTypes.forEach((words, i) => {
     const text = new SplitType(words, {types: 'words'})
@@ -16,5 +15,60 @@ splitTypes.forEach((words, i) => {
         },
         opacity: 0,
         stagger: 0.1
+    })
+})
+
+gsap.from(".bnr-l" , {
+    scrollTrigger: {
+        trigger: ".bnr-l",
+        start: 'top 80%',
+        end: 'bottom 50%',
+        scrub: true,
+        markers: false
+    },
+    x: -200,
+    opacity: 0.1
+})
+
+gsap.from(".bnr-r" , {
+    scrollTrigger: {
+        trigger: ".bnr-r",
+        start: 'top 80%',
+        end: 'bottom 50%',
+        scrub: true,
+        markers: false
+    },
+    x: 200,
+    opacity: 0.1
+})
+
+const pro = document.querySelectorAll(".pro");
+pro.forEach((hPro, i) => {
+    gsap.from(hPro, {
+        scrollTrigger: {
+            trigger: hPro,
+            start: 'top 80%',
+            end: 'top 20%',
+            scrub: true,
+            markers: false,
+        },
+        scale: 0.5,
+        opacity: 0.2
+    })
+})
+
+const wItem = document.querySelectorAll(".wl-item");
+console.log(wItem);
+wItem.forEach((wLi, i) => {
+    gsap.from(wLi, {
+        scrollTrigger: {
+            trigger: wLi,
+            start: 'top 80%',
+            end: 'bottom 80%',
+            scrub: true,
+            markers: false
+        },
+        x: -200,
+        opacity: 0
     })
 })
